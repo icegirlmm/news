ad2d06c31589ececa11f0c95e7af0f1cab3a52b4
icegirlmm <icegirlm@163.com>
2018-06-03 10:11:48 +0800

    foth commit

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-$(window).resize(function ()// 绑定到窗口的这个事件中
-{
-  var whdef = 100/1920;// 表示1920的设计图,使用100PX的默认值
-  var wH = window.innerHeight;// 当前窗口的高度
-  var wW = window.innerWidth;// 当前窗口的宽度
-  var rem = wW * whdef;// 以默认比例值乘以当前窗口宽度,得到该宽度下的相应FONT-SIZE值
-  $('html').css('font-size', rem + "px");
-});
+
+// let htmlWidth=document.documentElement.clientWidth || document.body.clientHeight;
+// console.log(htmlWidth)
+
+// let htmlDom=document.getElementsByTagName('html')[0];
+// htmlDom.style.fontSize=htmlWidth/10 +'px';
+
+// let hT=document.documentElement.clientHeight || document.body.clientHeight;//当前窗口的高度
+// console.log(hT);
+// $("#top").css("height","hT + 'px'");
+
+
+// $(window).resize(function ()// 绑定到窗口的这个事件中
+// {
+//   var whdef = 100/1920;// 表示1920的设计图,使用100PX的默认值
+//   var wH = window.innerHeight;// 当前窗口的高度
+//   var wW = window.innerWidth;// 当前窗口的宽度
+//   var rem = wW * whdef;// 以默认比例值乘以当前窗口宽度,得到该宽度下的相应FONT-SIZE值
+//   $('html').css('font-size', rem + "px");
+// });
 
 window.onload = function () {
     // var big =document.getElementById("big_wz");
